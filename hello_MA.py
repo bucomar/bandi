b8cd2f320d0cb56274c8e21e4c25a1f8e0ebea85
bucomar <59670765+bucomar@users.noreply.github.com>
2020-06-18 07:01:41 +0200

    2 program

---
+++ hello_MA.py
@@ -0,0 +1,7 @@
+# Bandi első python programja
+
+kalandozok = ':-) ;-) :-{'
+hullam = ' _/""\_ '
+print()
+
+print((hullam + kalandozok) * 5 + hullam)
---
+++ hosszu_mondat.py
@@ -0,0 +1,15 @@
+# Mondatba foglalos - Bandi 2. programja
+
+i = 1
+
+while i == 1 :
+	
+	#print('"kssaoiuzooyt }:-) :-]      -0-0-0- ow -0--0-----000-0--0  112812w745       wwwwwwwr yzi ry  " idezet Banditol')
+	print()
+
+	bolcsesseg = input('Irj ide valami bolcset: ')
+	print()
+	print('Egyszer egy bolcs ember azt mondta, hogy... " ' + bolcsesseg + ' " :-{')
+	print()
+
+	#print('"ghjf1 ds   dsa        dcp311i  " meg egy idezet Banditol ')
